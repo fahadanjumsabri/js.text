5a263809305278bce74827388c6aabe1535a0239
Fahad Anjum Sabri <146746171+fahadanjumsabri@users.noreply.github.com>
2023-10-04 12:45:11 +0000

    coversionOperation and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail = "fahad@gogle.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// const accountId = 2 not allowed
+accountEmail = "siraj@gogle.com"
+accountPassword = "12122121"
+accountCity="lahore"
+let acccountstate;
+
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,acccountstate])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// alert(3+3) we are using nodejs, not browser//
+
+console.log(3+3)//code readability should be high
+console.log("fahad");
+
+
+let name = "fahad"
+let age = 14
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigIn
+//string => ""
+//boolean=>true/false
+//null=>standalonevalue
+//undifined =>
+//symbol=>unique
+//object=>
+
+console.log(typeof undifined);
